bd9f821c507f59de4a158bc0b6730e287afc5a9c
AR Associates <ar.associates1957@gmail.com>
2025-04-04 22:39:31 +0530

    logo dimension

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -54,7 +54,7 @@ const Header = () => {
     <div className="bg-finance text-white p-4 shadow-md">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center mb-4 md:mb-0">
-          <img src="/images/AR Associates Logo.png" alt="My company Logo" className="h-8 md:h-10"/>
+          <img src="/images/AR Associates Logo.png" alt="My company Logo" className="h-20 md:h-15"/>
         </div>
         <div className="flex items-center justify-between w-full md:w-auto">
           <div className="text-sm opacity-80 mr-4">
